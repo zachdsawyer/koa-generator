b10e0ba2e9a73f41f7e0d08f8f947f70eef1503b
250ok-zach <zach@250ok.com>
2017-12-18 14:55:49 -0500

    customize template

--- templates2/js/app.js
+++ templates2/js/app.js
@@ -1,44 +1,52 @@
 const Koa = require('koa')
+const IO = require('socket.io')
 const app = new Koa()
+app.io = new IO();
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const less = require('koa-less2x')
 
 const index = require('./routes/index')
-const users = require('./routes/users')
+
+app.io.on('connection', (s) => {
+    console.log('connected');
+})
 
 // error handler
 onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+    enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
+app.use(less('./public', {
+    debug: true
+}))
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
-  extension: '{views}'
+    extension: '{views}'
 }))
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+app.use(async(ctx, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date() - start
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+    console.error('server error', err, ctx)
 });
 
 module.exports = app
